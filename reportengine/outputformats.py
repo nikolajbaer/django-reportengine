0f35dd0baa67706f5b0f426aeb1c01b1c47fe03c
dmpayton <derek.payton@gmail.com>
2010-09-01 11:43:19 -0700

    Added generate_report management command to create reports from the command line, required changes to outputformats to make it easier

--- reportengine/outputformats.py
+++ reportengine/outputformats.py
@@ -3,6 +3,7 @@ from django.template.context import RequestContext
 from django.http import HttpResponse
 from django.utils.encoding import smart_unicode
 import csv
+from cStringIO import StringIO
 from xml.etree import ElementTree as ET
 
 class OutputFormat(object):
@@ -10,12 +11,20 @@ class OutputFormat(object):
     slug="output"
     no_paging=False
 
+    def generate_output(self, context, output):
+        ## output is expected to be a file-like object, be it Django Response,
+        ## StringIO, file, or sys.stdout. Anything sith a .write method should do.
+        raise NotImplemented("Use a subclass of OutputFormat.")
+
     def get_response(self,context,request):
-        raise Exception("Use a subclass of OutputFormat.")
+        raise NotImplemented("Use a subclass of OutputFormat.")
 
 class AdminOutputFormat(OutputFormat):
     verbose_name="Admin Report"
     slug="admin"
+
+    def generate_output(self, context, output):
+        raise NotImplemented("Not necessary for this output format")
 
     def get_response(self,context,request):
         context.update({"output_format":self})
@@ -34,11 +43,8 @@ class CSVOutputFormat(OutputFormat):
         self.delimiter=delimiter
         self.lineterminator=lineterminator
 
-    def get_response(self,context,request):
-        resp = HttpResponse(mimetype='text/csv')
-        # CONSIDER maybe a "get_filename" from the report?
-        resp['Content-Disposition'] = 'attachment; filename=%s.csv'%context['report'].slug
-        w=csv.writer(resp,
+    def generate_output(self, context, output):
+        w=csv.writer(output,
                     delimiter=self.delimiter,
                     quotechar=self.quotechar,
                     quoting=self.quoting,
@@ -48,6 +54,13 @@ class CSVOutputFormat(OutputFormat):
         w.writerow( context["report"].labels)
         for r in context["rows"]:
             w.writerow([smart_unicode(x) for x in r])
+        return output
+
+    def get_response(self,context,request):
+        resp = HttpResponse(mimetype='text/csv')
+        # CONSIDER maybe a "get_filename" from the report?
+        resp['Content-Disposition'] = 'attachment; filename=%s.csv'%context['report'].slug
+        self.generate_output(context, resp)
         return resp
 
 class XMLOutputFormat(OutputFormat):
@@ -60,10 +73,7 @@ class XMLOutputFormat(OutputFormat):
         self.row_tag=row_tag
         self.aggregate_tag=aggregate_tag
 
-    def get_response(self,context,request):
-        resp = HttpResponse(mimetype='text/xml')
-        # CONSIDER maybe a "get_filename" from the report?
-        resp['Content-Disposition'] = 'attachment; filename=%s.xml'%context['report'].slug
+    def generate_output(self, context, output):
         root = ET.Element(self.root_tag) # CONSIDER maybe a nicer name or verbose name or something
         for a in context["aggregates"]:
             ae=ET.SubElement(root,self.aggregate_tag)
@@ -77,5 +87,11 @@ class XMLOutputFormat(OutputFormat):
                 e1=ET.SubElement(e,labels[l])
                 e1.text = r[l]
         tree=ET.ElementTree(root)
-        tree.write(resp)
+        tree.write(output)
+
+    def get_response(self,context,request):
+        resp = HttpResponse(mimetype='text/xml')
+        # CONSIDER maybe a "get_filename" from the report?
+        resp['Content-Disposition'] = 'attachment; filename=%s.xml'%context['report'].slug
+        self.generate_output(context, resp)
         return resp
